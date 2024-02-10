30b814fe0ec2a51a87d1855428d44d446ccf6001
zahid072 <jahidhasan19368@gmail.com>
2024-02-10 22:34:15 +0600

    update text with input value

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
-
 // multiple way of click event
 
 const h1 = document.getElementById("h1");
 
-function textChange(){
-    h1.innerText = "changed";
+function textChange() {
+  h1.innerText = "changed";
 }
-function textChange2(){
-    document.getElementById("h1").innerText = "Dom spacial event";
-    
+const Previous = () => {
+  document.getElementById("h1").innerText = "Dom spacial event";
 }
-const greenBtn = document.getElementById('makeGreen')
+const greenBtn = document.getElementById("makeGreen");
 
 greenBtn.onclick = makeGreen;
 
-function makeGreen(){
-    document.body.style.backgroundColor = "green";
-    h1.style.color = 'white'
+function makeGreen() {
+  document.body.style.backgroundColor = "green";
+  h1.style.color = "white";
 }
 
-const redBtn = document.getElementById('makeRed');
+const redBtn = document.getElementById("makeRed");
 
-redBtn.onclick = function makeRed(){
-    document.body.style.backgroundColor = "red"
-    h1.style.color = 'white'
-}
+redBtn.onclick = function makeRed() {
+  document.body.style.backgroundColor = "red";
+  h1.style.color = "white";
+};
 
-const yellowBtn = document.getElementById('makeYellow');
+const yellowBtn = document.getElementById("makeYellow");
 
-yellowBtn.addEventListener('click', ()=>{
-   document.body.style.backgroundColor = 'blue';
-   h1.style.color = 'white'
-})
+yellowBtn.addEventListener("click", () => {
+  document.body.style.backgroundColor = "blue";
+  h1.style.color = "white";
+});
 
+// text update with input value
 
+document.getElementById("Update").addEventListener("click", () => {
+  const input = document.getElementById("input").value;
 
+  document.getElementById("updateText").innerText = input;
 
+  document.getElementById("input").value = "";
+});
